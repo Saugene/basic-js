d99b76b5f9e9de29116d7749fd25cd78667d2126
Yauheni Kashlei <one.eugene.pacific@gmail.com>
2021-03-07 22:53:59 +0300

    feat: finish count-cats and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let s = "", n = "";
+  let k = 0;
+  for (let i = 0; i < members.length; i++) {
+    n = members[i];
+    if (typeof n !== "string") continue;
+    while (n[k] == " ") {
+      k++;
+    }
+    s += n[k];
+    k = 0;
+  }
+
+  s = s.toUpperCase().split("").sort().join("");
+  return s;
 };
