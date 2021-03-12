5c99f3efd475b46a11f91a4013c105ccab725df6
Yauheni Kashlei <one.eugene.pacific@gmail.com>
2021-03-13 01:03:25 +0300

    feat: finish carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,8 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+     if (!sampleActivity || typeof (sampleActivity) != "string" || !Number(sampleActivity) || Number(sampleActivity) <= 0 || Number(sampleActivity) > 15) return false;
+     let sA = Number(sampleActivity);
+     return Math.ceil(Math.log(MODERN_ACTIVITY/sA)*HALF_LIFE_PERIOD/0.693);
 };
