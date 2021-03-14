238b316de0b8fdda3d5d637ad9b8dadb10b924d8
Yauheni Kashlei <one.eugene.pacific@gmail.com>
2021-03-14 20:39:44 +0300

    feat: finish extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,23 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  if (typeof(str) != 'string') String(str);
-  if (typeof(options.addition) != 'string') String(options.addition);
-  if (!str) return false;
+  if (str === undefined) return false;
+    else String(str);
   if (!options.separator) options.separator = '+';
   if (!options.additionSeparator) options.additionSeparator = '|';
-  if (!options.addition) options.addition = '';
+  if (options.addition === undefined) options.addition = '';
+    else String(options.addition);
   if (!options.repeatTimes) options.repeatTimes = 0;
   if (!options.additionRepeatTimes) options.additionRepeatTimes = 0;
+  if (typeof(str) != 'string') String(str);
+  if (typeof(options.addition) != 'string') String(options.addition);
   
-  str += options.addition;
+  str += String(options.addition);
+
   
   for (let j = 0; j < Number(options.additionRepeatTimes) - 1; j++) {
       str += options.additionSeparator + options.addition;
   }
 
   let addString = str;
-  
+
   for (let i = 0; i < Number(options.repeatTimes) - 1; i++) {
       str += options.separator + addString;
   }
